4dbf44ef91eb8ec17479b64ebb71c790d64437d4
Kaz Watanabe <kwatanabe@heartbeats.jp>
2018-11-23 09:58:04 +0900

    artifactsを出力する処理を実装

--- create-build-shript.sh
+++ create-build-shript.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# usage 
+# create-build-script.sh REPOSITORY_URL REPOSITORY_NAME
+
 apis=(
     "test-app"
     "test-proxy"
@@ -16,7 +19,7 @@ for apiname in "${apis[@]}" ; do
     hash=`git -C ./${apiname} log --pretty=%H | head -n 1`
 
     echo "api = ${apiname}, hash = ${hash}"
-    result=`aws ecr list-images --repository-name $1 | grep $hash | wc -l`
+    result=`aws ecr list-images --repository-name $2 | grep $hash | wc -l`
 
     if [ $result -eq 0 ]; then
         echo "docker build --tag $1:${hash} ./${apiname}"
@@ -26,5 +29,8 @@ for apiname in "${apis[@]}" ; do
 
         echo "echo \"docker push $1:${hash}\"" >> /tmp/push.sh
         echo "docker push $1:${hash}" >> /tmp/push.sh
+
+        # 仮(複数APIのdeployに対応が必要)
+        echo "{\"tag\":\"${hash}\"}" > /tmp/build.json
     fi
 done

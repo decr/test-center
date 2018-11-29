12369be23c70df708eba31a687740f19b525e023
Kaz Watanabe <kwatanabe@heartbeats.jp>
2018-11-29 11:56:24 +0900

    ビルドスクリプトを分割

--- create-push-script.sh
+++ create-push-script.sh
@@ -3,7 +3,7 @@
 # usage 
 # create-build-script.sh REPOSITORY_URL REPOSITORY_NAME
 
-echo "#!/bin/bash" > /tmp/build.sh
+echo "#!/bin/bash" > /tmp/push.sh
 
 service=$1
 repo_url=$2
@@ -22,8 +22,8 @@ result=`aws ecr list-images --repository-name $repo_name | grep $hash | wc -l`
 if [ $result -eq 0 ]; then
     echo "docker build --tag $repo_url:${hash} ./${service}"
     
-    echo "echo \"docker build --tag $repo_url:${hash} ./${service}\"" >> /tmp/build.sh
-    echo "docker build --tag $repo_url:${hash} ./${service}" >> /tmp/build.sh
+    echo "echo \"docker push $1:${hash}\"" >> /tmp/push.sh
+    echo "docker push $repo_url:${hash}" >> /tmp/push.sh
 else
     echo "[${service}] Latest version image exists, build process was skipped."
 fi
---
+++ create-tags-json.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# usage 
+# create-build-script.sh REPOSITORY_URL REPOSITORY_NAME
+
+services=(
+    "test-app"
+    "test-proxy"
+)
+
+declare -A tags=()
+
+for service in "${services[@]}" ; do
+    if [ ! -d "./${service}" ]; then
+        echo "[${service}] Service directory not found"
+        continue
+    fi
+
+    hash=`git -C ./${service} log --pretty=%H | head -n 1`
+    tags[$service]=$hash
+done
+
+# 各コンテナの最新のtagをjson化して出力
+for key in "${!tags[@]}"; do
+    printf '%s\0%s\0' "$key" "${tags[$key]}"
+done |
+jq -Rs '
+  split("\u0000")
+  | . as $a
+  | reduce range(0; length/2) as $i 
+      ({}; . + {($a[2*$i]): ($a[2*$i + 1]|fromjson? // .)})' > /tmp/build.json
